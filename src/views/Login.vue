e8b0d928a617d448f7dbf0ed79df0d90cc8b73ce
Christophe HAMERLING <chamerling@linagora.com>
2020-07-27 13:06:58 +0200

    Implement Login view

---
+++ src/views/Login.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card class="elevation-6" tile max-width="500px">
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>OpenPaaS Login</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="Username"
+                name="username"
+                v-model="username"
+                prepend-icon="mdi-account"
+                type="text"
+              ></v-text-field>
+
+              <v-text-field
+                id="password"
+                label="Password"
+                name="password"
+                v-model="password"
+                prepend-icon="mdi-lock"
+                type="password"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="login">Login</v-btn>
+            <v-btn>Cancel</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { post } from "axios";
+
+export default {
+  data: () => ({
+    username: null,
+    password: null
+  }),
+  methods: {
+    login() {
+      post(
+        "/api/login",
+        {
+          username: this.username,
+          password: this.password,
+          rememberme: "false"
+        },
+        {
+          withCredentials: true
+        }
+      )
+        .then(() => {
+          window.location = `/${window.location.hash}`;
+        })
+        .catch(err => {
+          console.log("Error on login", err);
+        });
+    }
+  }
+};
+</script>
